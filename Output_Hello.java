0cecfe3473f6b0ca88af69d067a34a3b3ace69a3
1sherryjoy <154697402+1sherryjoy@users.noreply.github.com>
2025-01-23 00:18:30 +0300

    creating of new files

---
+++ Output_Hello.java
@@ -0,0 +1,20 @@
+/*  program code to output hello world
+ * Author:Joyce Njeri
+ * Reg No:CT101/G/19419/23
+ * Date:22nd Jan 2025
+ */
+ 
+//main class
+public class Main {
+    
+   //main method 
+    public static void main(String[] args){
+       //outputs hello world. 
+        System.out.println("Hello, World!");
+    }
+        
+        
+        
+   
+
+}
---
+++ Sum_Integers.java
@@ -0,0 +1,21 @@
+/*program code to Sum up two integers
+ * Author:Joyce Njeri
+ * Reg No: CT101/G/19419/23
+ * Date:22nd Jan 2025
+ */
+ 
+
+//main class
+public class SumOfInteger {
+    //main method
+    public static void main(String[]args){
+        //declaratuon and initialization of variable of integer type
+        int X=5;
+        int Y=6;
+        int sum =0;
+        sum =X+Y;
+        //output for sum of two integers
+        System.out.println("sum of two integers is :" +sum);
+    }
+    
+}
